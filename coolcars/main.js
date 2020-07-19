9c98e5e456e19029516b12af8856ef1e0463f7fd
paconovellino <pacognovellino@gmail.com>
2020-07-18 18:55:23 -0600

    Add files via upload
    cambios en js para capturar el formulario

--- coolcars/main.js
+++ coolcars/main.js
@@ -1,15 +1,49 @@
 (function(){
     ('use strict');
 
-    console.log(autos)
+    console.log(autos[0])
 
-var make = []
 
-var year = []
+var marca = []
+    for(var i = 0; i < autos.length; i++){
+    marca.push(autos[i].make)
+}
 
-var transmision = []
+var anio = []
+    for (var i = 0; i < autos.length; i++) {
+        anio.push(autos[i].year)
+    }
 
-var price = []
+var caja = []
+    for (var i = 0; i < autos.length; i++) {
+        caja.push(autos[i].transmission)
+    }
 
+var precio = []
+    for (var i = 0; i < autos.length; i++) {
+        precio.push(autos[i].price)
+    }
+
+
+var searchForm = []
+
+document.getElementById("button").onclick = function () {
+    var getBrand = document.getElementById("brand-form").value;
+    console.log(getBrand)
+    var getYear = document.getElementById("year-form").value;
+    console.log(getYear)
+    var getTrans = document.getElementById("trans-form").value;
+    console.log(getTrans)
+    var getPrice = document.getElementById("price-form").value
+    console.log(getPrice)
+}
+
+
+
+if(marca && anio && caja && precio === 1){
+
+}else{
+
+}
 
 })()
